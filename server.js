aaa68c05631719170dbaf9591d026cff12f03f6e
EballoFullStackDev <EBalloFullStackDev@gmail.com>
2020-04-15 15:09:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === 'production')
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
 }
-                                          /* Changed from 5000 to 5001 to avoid conflicts with Mern-Auth*/
+                                          
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`)); 
