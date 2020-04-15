53c0a2e6bf7650c03619f54889461256ebae3800
EballoFullStackDev <EBalloFullStackDev@gmail.com>
2020-04-15 15:04:46 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dbuser:dbpassword@cluster0-9mdky.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://dbuser:<dbpassword>@cluster0-9mdky.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey:"secret" //JSON WEB TOKEN
 }
 
-//    mongodb+srv://dbuser:<dbpassword>@cluster0-9mdky.mongodb.net/test?retryWrites=true&w=majority
  
