f487b90705139e74d9500a43d683077925911ff6
EballoFullStackDev <EBalloFullStackDev@gmail.com>
2020-06-06 11:29:22 -0700

    Update authActions.js

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -6,26 +6,26 @@ import {
   SET_CURRENT_USER,
   USER_LOADING, 
   CLEAR_ERRORS
-} from "./types"; //Constants
+} from "./types"; 
 
 
-export const registerUser = (userData, history) => dispatch => {  // Register User
+export const registerUser = (userData, history) => dispatch => {
   axios.post("/api/users/register", userData)
        .then(res => {
         dispatch({ type:CLEAR_ERRORS });
-        history.push("/login");         // re-direct to login on successful register
+        history.push("/login");
       }) 
        .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
 
-export const loginUser = userData => dispatch => { // Login - get user token
+export const loginUser = userData => dispatch => {
   axios.post("/api/users/login", userData)
        .then(res => {
-      const { token } = res.data;               // Save to localStorage  
-      localStorage.setItem("jwtToken", token);  // Set token to localStorage
-      setAuthToken(token);                      // Set token to Auth header
-      const decoded = jwt_decode(token);        // Decode token to get user data
-      dispatch(setCurrentUser(decoded));        // Set current user
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);    
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => dispatch({
         type: GET_ERRORS,
@@ -34,21 +34,20 @@ export const loginUser = userData => dispatch => { // Login - get user token
     );
 };
 
-export const setCurrentUser = decoded => {      // Set logged in user
+export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
   };
 };
 
-export const setUserLoading = () => {           // User loading
+export const setUserLoading = () => {
   return {
     type: USER_LOADING
   };
 };
 
-export const logoutUser = () => dispatch => {   // Log user out
-  localStorage.removeItem("jwtToken");          // Remove token from local storage
-  setAuthToken(false);                          // Remove auth header for future requests
-  dispatch(setCurrentUser({}));                 // Set current user to empty object {} which will set isAuthenticated to false
-};
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
